11776763570c34650ea2d09cfc3a587f17ea83ce
Scala Steward <me@scala-steward.org>
2021-10-15 14:23:42 +0200

    Update akka-actor, akka-testkit to 2.6.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
   val specs2Core = "org.specs2" %% "specs2-core" % "4.7.0"
   val specs2Mock = "org.specs2" %% "specs2-mock" % specs2Core.revision
-  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "[2.4,)"
+  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.6.17"
   val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % akkaActor.revision
   val specs2Matchers = "org.specs2" %% "specs2-matcher-extra" % specs2Core.revision
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
